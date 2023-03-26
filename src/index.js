466974c55579890cd92f2d3b31b450b164fb381c
kelvin901 <wawerukelvin2019@gmail.com>
2023-03-26 16:51:36 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-// write your code here
+const ramenMenu = document.querySelector('#ramen-menu');
+
+// Fetch the ramen data from the server
+fetch('http://localhost:3000/ramen')
+  .then(response => response.json())
+  .then(ramenData => {
+    // Iterate through each ramen object and display its image
+    ramenData.forEach(ramen => {
+      const ramenImg = document.createElement('img');
+      ramenImg.src = ramen.image;
+      ramenMenu.appendChild(ramenImg);
+    });
+  });
